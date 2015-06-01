cef89d9f4d829d6de2a68ee729b139d3d89aafd0
root <root@reductor_time_tests.(none)>
2015-06-01 19:42:59 +0500

    Добавил init скрипт + оно работает

---
+++ bin/plain2json.py
@@ -0,0 +1,15 @@
+import json
+import sys
+
+if len(sys.argv) != 2:
+    print "Usage: " + sys.argv[0] + " <plaintext file>"
+    exit(1)
+
+with open(sys.argv[1], 'r') as plaintext:
+    request_data = {}
+    request_data['name'] = 'timers'
+    request_data['columns'] = [ 'name', 'exectime', 'result' ]
+    request_data['points'] = []
+    for line in plaintext.readlines():
+        request_data['points'].append(line.strip().split(' '))
+    print json.dumps([ request_data ], indent=2)
